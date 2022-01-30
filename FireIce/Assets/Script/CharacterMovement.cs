9e7d30ffa218f0fddb04a816498c3557d3c89487
Installshi3ld <61904137+Installshi3ld@users.noreply.github.com>
2022-01-30 13:14:08 +0100

    Animation Done

--- FireIce/Assets/Script/CharacterMovement.cs
+++ FireIce/Assets/Script/CharacterMovement.cs
@@ -8,6 +8,9 @@ public class CharacterMovement : MonoBehaviour
     public bool activeCharacter;
     public float speed;
     public float rotationSpeed = 360;
+    public bool isMoving;
+
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,7 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (activeCharacter)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
@@ -28,12 +32,32 @@ public class CharacterMovement : MonoBehaviour
 
             transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
 
+            if (horizontalInput > 0 || verticalInput > 0 || horizontalInput < 0 || verticalInput < 0)
+            {
+                isMoving = true;
+            }
+            else
+            {
+                isMoving = false;
+            }
+
             if (movementDirection != Vector3.zero)
             {
                 transform.forward = movementDirection;
             }
+
+            if (isMoving == true)
+            { 
+                animator.SetTrigger("Walk");
+            }
+            else
+            {
+                animator.SetTrigger("IdleEvent");
+            }
             //transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
         }
+
+
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.F) || Input.GetKeyUp(KeyCode.Space))
         {
             Switch();
